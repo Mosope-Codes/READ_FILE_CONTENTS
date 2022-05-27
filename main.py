8f026ff8c3ff5e78bb9cc62ef35198f3de162dbd
Mosope-Codes <moshopeowo@gmail.com>
2022-05-27 06:58:38 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+from turtle import end_fill
+
+
+def read_file_content(filename):
+    # [assignment] Add your code here 
+    with open(filename, 'r') as read_file:
+        file_contents_as_string = read_file.read()
+    return file_contents_as_string
+
+do = read_file_content("story.txt")
+print(do)
+ 
+    
+
+new_dictionary = {}
+def count_words():
+    text = read_file_content("story.txt")
+    split_the_text_into_a_list = text.split(" ")
+    for word in split_the_text_into_a_list:
+        make_the_list_a_dictionary_with_the_key_as_count = {word: split_the_text_into_a_list.count(word)}
+        new_dictionary.update(make_the_list_a_dictionary_with_the_key_as_count)
+    print(new_dictionary)
+
+    
+
+    # [assignment] Add your code here
+count_words()
